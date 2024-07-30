b081734fa0082369753d5065a856f8030223c9c8
mosefatullah <mosefatullah@gmail.com>
2024-07-30 13:01:59 +0600

    .

--- src/data/schema.js
+++ src/data/schema.js
@@ -5,7 +5,7 @@ export const schema = {
  jobTitle: "Full Stack Web Developer",
  description:
   "Mohammad Sefatullah, a young and promising web designer, developer, and programmer, hails from the Barishal District of Bangladesh. Born on 2006, his passion for coding ignited while he was still in seventh grade. This early spark ignited a dream – to become a software engineer and make a positive impact on his community.",
- url: "https://sefatullah.me",
+ url: "https://sefatullah.pages.dev",
  image: "https://sefatullah.com/android-chrome-512x512.png",
  sameAs: [
   "https://www.facebook.com/mosefatullah",
